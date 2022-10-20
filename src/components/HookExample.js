35ba1851a8bcb35040544e45176799ef5b6ad8d6
DzmitryYesis <yesis1988@mail.ru>
2022-10-20 23:58:28 +0200

    Create Hooks component

---
+++ src/components/HookExample.js
@@ -0,0 +1,27 @@
+import {useEffect, useState} from "react";
+
+export const HookExample = () => {
+
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    const handleChange = (event) => {
+        setName(event.target.value)
+    }
+
+    useEffect(() => {
+        console.log(`HOOK. Компонент обновился...`)
+        document.title = `Вы нажали ${count} раз`
+    },[count])
+
+    return (
+        <div>
+            <input type={'text'} value={name} onChange={handleChange}/>
+            <button onClick={increment}>Click</button>
+        </div>
+    )
+}
